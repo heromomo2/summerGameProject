d6f6632b3d53c2b357080f75dc265d253ea979c9
Mohamud <detectivemomo1@gmail.com>
2016-08-24 00:06:18 -0400

    inition start

---
+++ Assets/Editor/GridEditor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+using System.Collections;
+
+[CustomEditor(typeof(Grid))]
+
+public class GridEditor : Editor {
+	Grid grid;
+
+
+	void OnEnable(){
+		grid = (Grid)target;
+	}
+
+	public override void OnInspectorGUI(){
+		//base.OnInspectorGUI ();
+		GUILayout.BeginHorizontal();
+		GUILayout.Label ("Grid Width");
+		grid.width = EditorGUILayout.Slider (grid.width, 1.0f, 100.0f, null);
+		GUILayout.EndHorizontal ();
+		
+	}
+	private float createslider(string LabelName,){
+	}
+
+}
